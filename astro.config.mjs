9f7fa5d4741137d449efcacfb5ce411b0138ca1b
Javier Moreno <javier.moreno23@alu.uclm.es>
2025-11-15 14:22:41 +0100

    Ajustes para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://javit-dev.github.io',
+    base: '/porfolio_javi/',
     vite: {
       plugins: [tailwindcss()]
     },
