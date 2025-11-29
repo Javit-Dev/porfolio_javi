77dcca982971f703c1683f6b65ecfd8bc423df69
Javier Moreno <javier.moreno23@alu.uclm.es>
2025-11-29 20:05:22 +0100

    Prototipo CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://javit-dev.github.io',
-    base: '/porfolio_javi/',
+    base: '/',
     vite: {
       plugins: [tailwindcss()]
     },
